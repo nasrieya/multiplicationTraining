069a8836abe97b2724b45b27ceca1c37cc9eeb9c
eya nasri <eyanasri@think-it.com>
2021-05-26 13:17:39 +0100

    Initial Commit

---
+++ MultiplicationPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplicationPractice
+//
+//  Created by Eya Nasri on 26/5/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiplicationPractice/MultiplicationPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationPracticeApp.swift
+//  MultiplicationPractice
+//
+//  Created by Eya Nasri on 26/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
